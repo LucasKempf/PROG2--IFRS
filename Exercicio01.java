0f07d5bbf1f2c0cc95b13f926d91087c0edd2200
LucasKempf <lucaskempf217@gmail.com>
2026-07-30 07:24:36 -0300

    Add files via upload

---
+++ Exercicio01.java
@@ -0,0 +1,27 @@
+package atividades;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercicio01 {
+	
+	public static void main(String[] args) {
+		
+		Scanner Leitor = new Scanner (System.in);
+		
+		ArrayList<Produto> produtos = new ArrayList<Produto>();
+		
+		int opcao = 0;
+		
+		System.out.println("-----MENU-----");
+		System.out.println("[ 1 ] Inserir Produtos no Sistema");
+		System.out.println("[ 2 ] Mostrar Produtos do Sistema");
+		System.out.println("[ 3 ] Sair de Sistema");
+		
+		
+		
+		
+		Leitor.close();
+	}
+
+}
